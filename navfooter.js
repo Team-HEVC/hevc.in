86f1458201ed598fa2b5624e18e9216a2137ead8
Anas Khan <anaskhaaan.28@gmail.com>
2023-03-24 00:50:05 +0530

    first commit

---
+++ navfooter.js
@@ -0,0 +1,10 @@
+const i1=document.querySelector(".i1");
+    const i2=document.querySelector(".i2");
+    function changearrow(){
+        i1.classList.add("none");
+        i2.classList.remove("none");
+    }
+    function changearrow1(){
+        i2.classList.add("none");
+        i1.classList.remove("none");
+    }
